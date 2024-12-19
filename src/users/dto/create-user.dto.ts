843e0d77fb03a13cb06ab4ba89cdbe95b22db4de
Ben Aguirre <baguirre@luc.edu>
2024-12-19 15:51:17 -0500

    add: hashing function into user.service

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateUserDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty({ required: false })
+  description: string;
+
+  @ApiProperty()
+  lattitude: number;
+
+  @ApiProperty()
+  longitude: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  password: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+export class UserEntity implements User {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  lattitude: number;
+
+  @ApiProperty()
+  longitude: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  password: string;
+}
+
+/*   id            String   @id @default(uuid())
+  username      String
+  name          String
+  description   String?
+  lattitude     Float?
+  longitude     Float?
+  createdAt     DateTime @default(now())
+  updatedAt     DateTime @updatedAt
+  password      String
+  hostedParties Party[]  @relation("PartyHost")
+  parties       Party[]  @relation("PartyMembers")*/
