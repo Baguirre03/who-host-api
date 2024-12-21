fbb33e5fd846c98690f03bffc36afb54824b6bc9
Ben Aguirre <baguirre@luc.edu>
2024-12-21 15:25:26 -0500

    add validation to route handlers

--- src/parties/dto/create-party.dto.ts
+++ src/parties/dto/create-party.dto.ts
@@ -1,28 +1,55 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { $Enums } from '@prisma/client';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreatePartyDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(60)
   @ApiProperty()
   name: string;
 
-  @ApiProperty()
-  id: string;
-
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
-  description: string;
+  description?: string;
 
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty()
   @ApiProperty()
   time: Date;
 
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty()
   @ApiProperty()
   createdAt: Date;
 
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty()
   @ApiProperty()
   updatedAt: Date;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   hostId: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   @ApiProperty({ required: false })
   status: $Enums.PartyStatus;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,26 +1,58 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsLatitude,
+  IsLongitude,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(60)
   @ApiProperty()
   username: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   @ApiProperty()
   name: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
-  description: string;
+  description?: string;
 
+  @IsNotEmpty()
+  @IsLatitude()
   @ApiProperty()
   lattitude: number;
 
+  @IsNotEmpty()
+  @IsLongitude()
   @ApiProperty()
   longitude: number;
 
+  @IsDate()
+  @IsNotEmpty()
   @ApiProperty()
   createdAt: Date;
 
+  @IsDate()
+  @IsNotEmpty()
   @ApiProperty()
   updatedAt: Date;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   password: string;
 }
